807b1725120096e5e2dfc4e51841972e137598c9
Thenuri Seleena <tseleena200@gmail.com>
2025-04-29 19:46:05 +0400

    Initial full project commit

---
+++ android/app/src/main/kotlin/com/example/onlinegroceries/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.onlinegroceries
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'common/color_extension.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Online Groceries',
+      theme: ThemeData(
+        fontFamily: "Gilroy",
+        colorScheme: ColorScheme.fromSeed(seedColor: TColor.primary),
+        useMaterial3: false,
+      ),
+      home: Container(),
+    );
+  }
+}
