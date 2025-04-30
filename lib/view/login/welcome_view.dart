0e0345053d037b93178312d0f7dde95ba3ef853e
Thenuri Seleena <tseleena200@gmail.com>
2025-04-30 10:31:49 +0400

    feat: completed welcome screen

--- lib/view/login/welcome_view.dart
+++ lib/view/login/welcome_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:onlinegroceries/common_widget/round_button.dart';
 
 class WelcomeView extends StatefulWidget {
   const WelcomeView({super.key});
@@ -16,12 +17,62 @@ class _WelcomeViewState extends State<WelcomeView> {
       body: Stack(
         children: [
           Image.asset(
-            "assets/img/welcom_bg.png",
+            "assets/img/welcomescreen.jpeg",
             width: media.width,
             height: media.height,
             fit: BoxFit.cover,
+            alignment: Alignment(0.13, 0),
+
+
+
+
+
           ),
 
+          SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+              Image.asset("assets/img/app_logo.png",width: 60,height: 60,),
+                const SizedBox(height: 8,), //space
+            //First Row
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                     Text(
+                      "   Welcome\n to SmartCart",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 48,
+                        fontWeight: FontWeight.w600,),
+                    ),
+                  ],
+                ),
+            
+                //Second Row
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Scan. Shop. Go.",
+                      style: TextStyle(
+                        color: Color(0xFFFCFCFC).withAlpha(178),
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 20,),//space
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: RoundButton(title: "Start Shopping", onPressed: (){
+
+              },),
+            ),
+                const SizedBox(height: 46,),//space between the text and buttons
+              ],
+            ),
+          ),
         ],
       ),
     );
